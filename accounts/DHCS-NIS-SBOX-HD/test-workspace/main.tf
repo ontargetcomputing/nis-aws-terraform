e9bab35774ed762be1ad353f87ac21fb9752614b
balutbomber <balutbomber@gmail.com>
2025-02-21 13:42:03 -0800

    fix

--- accounts/DHCS-NIS-SBOX-HD/test-workspace/main.tf
+++ accounts/DHCS-NIS-SBOX-HD/test-workspace/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-west-2"
 }
 resource "aws_s3_bucket" "vulnerable_bucket" {  
-    bucket = "rb-test-13"  
+    bucket = "rb-test-131"  
     acl    = "public-read" 
     
     versioning {    
