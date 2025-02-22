5b74670b383b9fcb47fdcbcb8c0bd1f605d89c50
Richard Bach <balutbomber@gmail.com>
2025-02-21 23:15:01 -0600

    fix (#13)
    * fix
    
    * fix

--- accounts/DHCS-NIS-SBOX-HD/test-workspace/main.tf
+++ accounts/DHCS-NIS-SBOX-HD/test-workspace/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-west-2"
 }
 resource "aws_s3_bucket" "vulnerable_bucket" {  
-    bucket = "rb-tes12"  
+    bucket = "rb-tes123"  
     acl    = "public-read" 
     
     versioning {    
