9f5ef2c841f75135728c5cef1755779d71ec15fb
balutbomber <balutbomber@gmail.com>
2025-02-24 10:42:59 -0800

    fix

--- accounts/DHCS-NIS-DEV-HD/test-workspace/provider.tf
+++ accounts/DHCS-NIS-DEV-HD/test-workspace/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = var.aws_region
   assume_role {
-    role_arn     = "arn:aws:iam::320812455017:role/Terraform-Workspaces-Centralized-IdentityProvider-Target-Role"
+    role_arn     = "arn:aws:iam::582983609964:role/balutbomber-terraform-admin-role"
     session_name = "terraform_cloud"
   }
 
