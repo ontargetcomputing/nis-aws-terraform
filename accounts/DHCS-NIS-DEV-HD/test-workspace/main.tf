3f56e3556986bcd3612dc38ad742edacb7438737
balutbomber <balutbomber@gmail.com>
2025-02-24 19:55:08 -0800

    fix

--- accounts/DHCS-NIS-DEV-HD/test-workspace/main.tf
+++ accounts/DHCS-NIS-DEV-HD/test-workspace/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "example" {
   name        = "/myapp/database/password"
-  description = "Database password for abc223456"
+  description = "Database password for abc2234567"
   type        = "SecureString"
   value       = "MySuperSecretPassword"
 
