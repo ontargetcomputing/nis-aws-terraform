ba347b108a91e2da18a309792fd9b9c3ed0cebb3
Richard Bach <balutbomber@gmail.com>
2025-02-24 22:05:06 -0600

    Fix4
    ## Description
    _Provide a brief summary of the changes included in this pull request._
    
    ## Related Issues
    _Mention any SNOW or Jira tickets this PR is related to._
    
    
    
    Comment: help me
    
    Closes #27
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- accounts/DHCS-NIS-DEV-HD/test-workspace/main.tf
+++ accounts/DHCS-NIS-DEV-HD/test-workspace/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "example" {
   name        = "/myapp/database/password"
-  description = "Database password for abc2234567"
+  description = "Database password for abc22345678"
   type        = "SecureString"
   value       = "MySuperSecretPassword"
 
